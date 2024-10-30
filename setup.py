4dc6f6f2fced464681a0a58b7d8af03f4262a8fa
mfairread <167294223+mfairread@users.noreply.github.com>
2024-10-30 19:05:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="cocosplit",
     version="0.2.0",
-    packages=find_packages(), 
+    packages=find_packages("source"), 
+    package_dir={"": "source"},
     entry_points={
         'console_scripts': [
-            'cocosplit=source.__main__:main',  
+            'cocosplit=coco_split.__main__:main',  
         ],
     },
     python_requires='>=3.10',  
